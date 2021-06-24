f0bfd48c9904a81cd5c01cfe812cefa066cb1269
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-06-23 21:34:46 -0400

    adding my user authentication route

---
+++ Routes/api/index.js
@@ -0,0 +1,18 @@
+//path allows the return of the path of the folder where the current JS files resides
+const path = require("path");
+//express is used and called to provide the methods like GET, POST, USE etc...
+// to be called for an HTTP or URL Pattern. 
+const router = require("express").Router();
+//this calls the user file where all the authentication process is at, for login, signup, so...
+//when loggin in you can login or registesr which will give you the result of logging in 
+const userRoutes = require("./users")
+
+//User Routes 
+router.use("/users", userRoutes);
+//For anything else, render the html page
+router.use(function(req, res) {
+   //this dirname is allowed to be used cause of path 
+   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+module.exports = router;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-
+//all the imports
 import './App.css';
 
+//the function that has that html file render for the user
 function App() {
   return (
     <div className="App">
