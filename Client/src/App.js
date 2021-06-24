3ba0fe33ea79296319585f6a3162dc72c5e95ee3
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-06-23 23:28:42 -0400

    adding the appjs file with the routes for the componetns and pages

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,80 @@
-//all the imports
+import React from 'react'
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch
+} from 'react-router-dom';
+import Auth from "./utils/Auth";
+import Nav from "./components/Nav";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { Container } from "./components/Grid";
+import PublicRoute from "./pages/PublicRoute";
+import ProtectedRoute from "./pages/ProtectedRoute";
 import './App.css';
+import { UserProvider } from "./utils/UserContext";
 
-//the function that has that html file render for the user
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
-  );
-}
+//everything that appears to the user, the pages/components, and the routes
+const AuthExample = () => (
+	<UserProvider>
+		<Router>
+			<div>
+				<Nav className="App-header" />
+				<Container>
+					<Switch>
+						<Route path="/public" component={PublicRoute} />
+						<Route path="/login" component={Login} />
+						<Route path="/register" component={Register} />
+						<PrivateRoute path="/protected" component={ProtectedRoute} />
+						{/* <Route component={NoMatch} /> */}
+					</Switch>
+				</Container>
+			</div>
+		</Router>
+	</UserProvider>
+)
 
-export default App;
+
+
+
+// This is the private route component this checks for an authorized user here
+const PrivateRoute = ({ component: Component, ...rest }) => (
+	<Router>
+		<div>
+
+			<Route {...rest} render={props => (
+
+				Auth.isAuthenticated ? (
+					<Component {...props} />
+				) : (
+						<div className="container">
+							<div className="alert alert-danger text-center" role="alert">
+								This page is private to authenticated users.
+					</div>
+							<div className="row">
+								<div className="col-sm"></div>
+								<div className="col-sm">
+									<h3>Please Register or Login</h3>
+								</div>
+								<div className="col-sm"></div>
+							</div>
+							<Redirect to={{
+								pathname: '/login',
+								state: { from: props.location }
+							}} />
+						</div>
+					)
+			)} />
+		</div>
+	</Router>
+)
+
+
+
+
+
+
+
+
+export default AuthExample
