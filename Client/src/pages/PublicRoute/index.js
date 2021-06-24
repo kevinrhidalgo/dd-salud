c9b85d8a363bbafb9f1efbf75f1405690ebd7b7b
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-06-23 23:40:40 -0400

    adding a publicpage

---
+++ Client/src/pages/PublicRoute/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+
+function PublicRoute() {
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-sm">
+                </div>
+                <div className="col-sm">
+                    <h3>Example Public Route</h3>
+                    <h6>Anyone can see this content</h6>
+                </div>
+                <div className="col-sm">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default PublicRoute
