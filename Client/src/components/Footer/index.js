8c2fc6d2c3c0f75a6d889444d97be6b00793a3cb
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-06-26 16:06:37 -0400

    adding my footer details

--- Client/src/components/Footer/index.js
+++ Client/src/components/Footer/index.js
@@ -3,7 +3,19 @@ import "./Footer.css";
 
 const footerBg={
   backgroundColor:'white',
-  height:'200px'
+  height:'200px',
+  display:'flex',
+  justifyContent:'center'
+}
+const footerInfo={
+ color:'#222222',
+ margin:'30px',
+ fontSize:'15px',
+ fontWeight:'bold',
+ fontFamily: 'Old Standard TT, serif'
+}
+const myTitle={
+  fontFamily:'Lobster Two, cursive',
 }
 
 function Footer() {
@@ -12,8 +24,13 @@ function Footer() {
   return (
     <div className='myFooter'>
     <nav style={footerBg} className="navbar navbar-expand  ">
-      
-      
+      <div className="footerInfo">
+      <p style={footerInfo}>Collaborate with Us</p>
+  <p style={footerInfo}>Contact Us </p>   
+    <p style={footerInfo}>Salud.</p>
+    <p style={footerInfo}>FAQ</p>
+   <p style={footerInfo}> © <span style={myTitle}>Salud</span> 2021</p>
+   </div>
     </nav>
     </div>
   );
