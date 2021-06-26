8d55dea0e4d2f859cea072d563b41bf83a2b18df
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-06-25 21:47:01 -0400

    css for login card

--- Client/src/components/LoginForm/index.js
+++ Client/src/components/LoginForm/index.js
@@ -7,8 +7,23 @@ function LoginForm({ onLogin }) {
 	const userNameRef = useRef();
 	const passwordRef = useRef();
 
+	const registerBtn={
+		borderRadius:'7%',
+		backgroundColor:'#e5d3b3',
+		fontSize:'12px',
+		padding:'5px',
+		float:'right',
+		border:'#e5d3b3 1px solid ',
+	}
+	const signupCard={
+		marginTop:'20px'
+	}
+
 	return (
-		<Card title="Login with your Username and Password">
+		<div className='carderM'>
+		<div className="registerCard">
+		<h1>Login</h1>
+		<p>Welcome Back!</p>
 			<form
 				ref={formRef}
 				onSubmit={(e) => {
@@ -19,15 +34,16 @@ function LoginForm({ onLogin }) {
 					});
 				}}
 			>
-				<div className="form-group">
+				<div style={signupCard} className="form-group">
 					<input className="form-control" ref={userNameRef} type='text' name="username" placeholder='Enter Username' /><br />
 					<input className="form-control" ref={passwordRef} type='password' name="password" placeholder='Password' /><br />
-					<button className="btn btn-success" type='submit'>
+					<button style={registerBtn} className="btn btn-success" type='submit'>
 						Submit
 						</button>
 				</div>
 			</form>
-		</Card>
+			</div>
+		</div>
 	)
 }
 
