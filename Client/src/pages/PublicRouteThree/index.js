4cce806f333e3a0aa6debea9bc2caff836e8ba64
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-06-27 13:51:38 -0400

    editing my vids page

--- Client/src/pages/PublicRouteThree/index.js
+++ Client/src/pages/PublicRouteThree/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import './Film.css'
+import ReactPlayer from 'react-player'
+
+const videoOne={
+  display:'flex',
+  justifyContent:'center'
+}
+
 
 
 //simple page that is public that does not need authentication
@@ -12,6 +19,10 @@ function PublicRouteThree() {
         <div class="vl"></div>
         <div><h1 className='videos'>THE FILMS</h1></div>
         <div class="vl"></div>
+        </div>
+        <div className='videoOne'>
+        <ReactPlayer style={videoOne} url='https://www.youtube.com/watch?v=3xW2uv_VsqQ'
+    />
         </div>
       </> 
     )
