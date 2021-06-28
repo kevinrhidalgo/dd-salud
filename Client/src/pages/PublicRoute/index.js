96262c78ef18e650853a6228083ece36cb9da227
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-06-28 16:21:03 -0400

    front page cards added

--- Client/src/pages/PublicRoute/index.js
+++ Client/src/pages/PublicRoute/index.js
@@ -3,7 +3,7 @@ import './Home.css'
 
 const quote={
   color:'white',
-  fontSize:'17px',
+  fontSize:'18px',
   fontFamily: 'Benne, serif',
   display:'flex',
   justifyContent:'center',
@@ -11,15 +11,34 @@ const quote={
 }
 const quoter={
   fontStyle:"italic",
-  color:"teal"
+  color:"teal",
+  marginTop:'0px'
 }
+const workingTitle={
+  display:'flex',
+  justifyContent:'center',
+  fontFamily:'Lobster Two, cursive',
+  fontSize:'30px',
+  paddingTop:'20px'
+}
+const middleInfo={
+  display:'flex',
+  justifyContent:'center',
+  color:'#222222',
+  margin:'30px',
+  fontSize:'15px',
+  fontWeight:'bold',
+  fontFamily: 'Old Standard TT, serif',
+  textDecoration:'none'
+ }
+ 
 //simple page that is public that does not need authentication
 
 function PublicRoute() {
 
     return (
         <div className='homePage'>
-
+<div className='homeBackground'>
           <div className='home'>
       <div style ={quote} className='daQuotes'>
         <div className='quoteColumn'>
@@ -28,6 +47,64 @@ function PublicRoute() {
       </div> 
       </div>
       </div>
+  </div> 
+
+  <div className='middleSection'>
+  <h1 style={workingTitle}>About us.</h1>
+  <p style={middleInfo}>Salud celebrates healthy lifestyles. 
+  From recipes, <br/> to workouts, to how 
+  you life or view life. 
+  </p>
+  </div>
+
+    <div className='homeBody'>
+
+    <div className='vidHeader'>
+        <div class="vl"></div>
+        <div><h1 className='blog'>STORIES</h1></div>
+        <div class="vl"></div>
+    </div>
+
+<div className='storySecOne'>
+
+<div className='storyUno'>
+<div className='partUno'></div>  
+<div className='partDos'></div>
+</div> 
+
+<div className='storyDos'>
+<div className='partUno'></div>  
+<div className='partDos'></div>
+</div> 
+</div>
+
+<div className='storySecOne'>
+
+<div className='storyUno'>
+<div className='partUno'></div>  
+<div className='partDos'></div>
+</div> 
+
+<div className='storyDos'>
+<div className='partUno'></div>  
+<div className='partDos'></div>
+</div> 
+</div>
+
+<div className='storySecOne'>
+
+<div className='storyUno'>
+<div className='partUno'></div>  
+<div className='partDos'></div>
+</div> 
+
+<div className='storyDos'>
+<div className='partUno'></div>  
+<div className='partDos'></div>
+</div> 
+</div>
+      
+    </div>
 
 
 
