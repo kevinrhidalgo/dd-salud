56464030a69d0564b5336089c696994583415a66
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-06-29 02:33:17 -0400

    starting the first blog page

--- Client/src/pages/BlogOne/index.js
+++ Client/src/pages/BlogOne/index.js
@@ -5,6 +5,9 @@ function BlogOne() {
     return (
 		<>
 		<h1>How going vegan changed my life.</h1>
+		<p>
+		What is the first thing when you hear the phrase, "I am vegan".......
+		</p>
 		</>
 		)
 	}
